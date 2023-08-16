303da7ba268ed99848789644f90899dc7aa4ae06
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2023-08-16 19:46:47 -0300

    feat: configurando prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-function App() {
-  
-    return (
-        <>
-            <h1>Vite + React</h1>
-        </>
-    )
-}
+const App = () => {
+  return <h1>Vite + React</h1>;
+};
 
 export default App;
